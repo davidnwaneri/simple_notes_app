3652cd288bb8dadc723c68719996123d3826a25e
David Nwaneri <davidn4333@gmail.com>
2023-04-02 19:51:07 +0100

    feat: setup bottom navigation bar

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,13 +1,34 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:simple_notes_app/main.dart';
+import 'package:simple_notes_app/view/view.dart';
 
 class AppRouter {
+  static const _initialLocation = '/notes';
+  static final _rootNavigatorKey = GlobalKey<NavigatorState>();
+  static final _shellNavigatorKey = GlobalKey<NavigatorState>();
+
   final GoRouter router = GoRouter(
+    initialLocation: _initialLocation,
+    navigatorKey: _rootNavigatorKey,
     routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) {
-          return const MyHomePage();
+      ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        routes: [
+          GoRoute(
+            path: '/notes',
+            builder: (context, state) {
+              return const HomeScreen();
+            },
+          ),
+          GoRoute(
+            path: '/settings',
+            builder: (context, state) {
+              return const SettingsScreen();
+            },
+          ),
+        ],
+        builder: (context, state, child) {
+          return BottomNavigationScaffold(child: child);
         },
       ),
     ],
---
+++ lib/view/src/bottom_navigation_scaffold.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class BottomNavigationScaffold extends StatefulWidget {
+  const BottomNavigationScaffold({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  State<BottomNavigationScaffold> createState() =>
+      _BottomNavigationScaffoldState();
+}
+
+class _BottomNavigationScaffoldState extends State<BottomNavigationScaffold> {
+  int get _currentIndex {
+    final location = GoRouter.of(context).location;
+    final index = _locationToIndex(location);
+    return index;
+  }
+
+  int _locationToIndex(String location) {
+    switch (location) {
+      case '/settings':
+        return 1;
+      default:
+        return 0;
+    }
+  }
+
+  void _toggleCurrentIndex(int nextIndex) {
+    if (nextIndex == _currentIndex) return;
+    if (nextIndex != 1) {
+      context.go('/notes');
+    } else {
+      context.go('/settings');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widget.child,
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: _toggleCurrentIndex,
+        destinations: const <NavigationDestination>[
+          NavigationDestination(
+            icon: Icon(Icons.sticky_note_2),
+            label: 'Notes',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings_suggest),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
