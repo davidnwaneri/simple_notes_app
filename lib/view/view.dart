be3cf16fd46977109dad4a7b6bfaf49833c5ff4a
David Nwaneri <davidn4333@gmail.com>
2023-04-02 21:38:37 +0100

    refactor: move notes directory directly inside the `view` directory

--- lib/view/view.dart
+++ lib/view/view.dart
@@ -1,4 +1,4 @@
 // Barrel file for view library
+export 'package:simple_notes_app/view/notes/notes_screen.dart';
 export 'package:simple_notes_app/view/src/bottom_navigation_scaffold.dart';
-export 'package:simple_notes_app/view/src/notes/notes_screen.dart';
 export 'package:simple_notes_app/view/src/settings_screen.dart';
