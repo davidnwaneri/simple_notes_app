96aa50eac7b9a1975aa34596b9c90b8407909b36
David Nwaneri <davidn4333@gmail.com>
2023-04-10 13:49:16 +0100

    refactor: move `setting_screen.dart` to settings folder

--- lib/view/view.dart
+++ lib/view/view.dart
@@ -5,5 +5,5 @@ export 'package:simple_notes_app/view/notes/edit_note_screen.dart';
 export 'package:simple_notes_app/view/notes/note_details_screen.dart';
 export 'package:simple_notes_app/view/notes/notes_screen.dart';
 export 'package:simple_notes_app/view/profile/profile_screen.dart';
+export 'package:simple_notes_app/view/settings/settings_screen.dart';
 export 'package:simple_notes_app/view/src/bottom_navigation_scaffold.dart';
-export 'package:simple_notes_app/view/src/settings_screen.dart';
