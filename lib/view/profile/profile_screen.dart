75037577cb461a2856a555581b7f81ec184c07d0
David Nwaneri <davidn4333@gmail.com>
2023-04-17 17:14:04 +0100

    show user info in profile

--- lib/view/profile/profile_screen.dart
+++ lib/view/profile/profile_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:simple_notes_app/view/authentication/authentication.dart';
 import 'package:simple_notes_app/view/profile/profile_screen_widgets.dart';
+import 'package:simple_notes_app/view/profile/widgets/profile_card.dart';
 import 'package:simple_notes_app/widgets_library/widgets_library.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -18,7 +21,14 @@ class ProfileScreen extends StatelessWidget {
             sliver: SliverList(
               delegate: SliverChildListDelegate(
                 [
-                  const ProfileCard(),
+                  BlocBuilder<AuthBloc, AuthState>(
+                    builder: (context, state) {
+                      return state.maybeWhen(
+                        orElse: () => const ProfileCard(user: null),
+                        signedIn: (user) => ProfileCard(user: user),
+                      );
+                    },
+                  ),
                   const SizedBox(height: 30),
                   const ItemTilesArea(),
                 ],
---
+++ lib/view/profile/widgets/profile_card.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:simple_notes_app/core/extensions.dart';
+import 'package:simple_notes_app/models/models.dart';
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({
+    required this.user,
+    super.key,
+  });
+
+  final User? user;
+
+  @override
+  Widget build(BuildContext context) {
+    final userIsSignedIn = user != null;
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 8,
+          vertical: 32,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CircleAvatar(
+              child: Icon(
+                Icons.account_circle_rounded,
+              ),
+            ),
+            const SizedBox(width: 10),
+            if (userIsSignedIn)
+              SignedInUserProfileInfo(user: user!)
+            else
+              const UserNotSignedInProfileInfo(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignedInUserProfileInfo extends StatelessWidget {
+  const SignedInUserProfileInfo({
+    required this.user,
+    super.key,
+  });
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          user.name,
+          style: context.theme.textButtonTheme.style?.textStyle?.resolve({
+            ...MaterialState.values,
+          }),
+        ),
+        Text(user.email),
+      ],
+    );
+  }
+}
+
+class UserNotSignedInProfileInfo extends StatelessWidget {
+  const UserNotSignedInProfileInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextButton(
+          style: TextButton.styleFrom(
+            padding: EdgeInsets.zero,
+            minimumSize: Size.zero,
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+          onPressed: () {
+            context.push('/auth/sign-in');
+          },
+          child: Text(
+            'Sign In',
+            style: context.theme.textButtonTheme.style?.textStyle?.resolve({
+              ...MaterialState.values,
+            })?.copyWith(
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ),
+        const Text(
+          'Back up, Sync',
+        ),
+      ],
+    );
+  }
+}
