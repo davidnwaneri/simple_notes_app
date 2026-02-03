4b0a6655006736f30bb43389c26693e500204b17
David Nwaneri <48889672+davidnwaneri@users.noreply.github.com>
2026-02-03 16:37:38 +0100

    recreate android folder

---
+++ android/app/src/main/kotlin/app/simplenotes/davidnwaneri/MainActivity.kt
@@ -0,0 +1,5 @@
+package app.simplenotes.davidnwaneri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/create_video___simple_notes_app_with_appwrite_davidnwaneri/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.create_video___simple_notes_app_with_appwrite_davidnwaneri
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
